9883f5bfe17685de63f65ce5cf33c4e5d5793b8a
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-04-26 15:23:39 -0300

    Logica de Home Page (revisar)

--- Ecommerce/Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Ecommerce/Controllers/HomeController.cs
@@ -2,8 +2,31 @@
 
 namespace Ecommerce.Controllers
 {
-    public class HomeController : Controller /*_BaseController*/
+    public class HomeController : Controller
     {
+    
+        //private readonly ProductsManagerContext context;
+
+        //public HomeController(ProductsManagerContext productsManagerContext)
+        //{
+        //    this.context = productsManagerContext;
+        //}
+
+
+        //public IActionResult Index()
+        //{
+        //    HomeViewModel model = new HomeViewModel();
+
+        //    List<Lote> lots = context.Lote.Where(x => x.Estado == Lote.EstadoLote.ENABLED).OrderByDescending(x => x.Id).ToList();
+
+        //    model = new HomeViewModel
+        //    {
+        //        Lots = lots,
+        //    };
+
+        //    return View(model);
+        //}
+        
         public IActionResult Index()
         {
             return View();
