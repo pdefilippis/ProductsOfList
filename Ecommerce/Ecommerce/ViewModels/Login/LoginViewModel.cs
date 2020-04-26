508bb864eb88aaf204c45f70c4834b44ef890f40
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-04-26 19:47:56 -0300

    Se agrego en viewmodel Surname y Name

--- Ecommerce/Ecommerce/ViewModels/Login/LoginViewModel.cs
+++ Ecommerce/Ecommerce/ViewModels/Login/LoginViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -14,20 +14,20 @@ namespace Ecommerce.ViewModels.Login
 
         public class InputModel
         {
-            [Display(Name = "Email")]
             [Required(ErrorMessage = "<font color='red'>El campo 'Email' es obligatorio.</font>")]
             [EmailAddress(ErrorMessage = "<font color='red'>El 'Email' no es una dirección de Email válida.</font>")]
             public string Email { get; set; }
-
-            [Display(Name = "Contraseña")]
             [Required(ErrorMessage = "<font color='red'>El campo 'Contraseña' es obligatorio.</font>")]
             [DataType(DataType.Password)]
             [StringLength(100, ErrorMessage = "<font color='red'>El número de caracteres del {0} debe ser al menos {2}.</font>", MinimumLength = 6)]
             public string Password { get; set; }
-
-            [Display(Name = "Usuario")]
             [Required(ErrorMessage = "<font color='red'>El campo 'Usuario' es obligatorio.</font>")]
             public string User { get; set; }
+            [Required(ErrorMessage = "<font color='red'>El campo 'Apellido' es obligatorio.</font>")]
+            public string Surname { get; set; }
+            [Required(ErrorMessage = "<font color='red'>El campo 'Nombre' es obligatorio.</font>")]
+            public string Name { get; set; }
+
         }
     }
 }
