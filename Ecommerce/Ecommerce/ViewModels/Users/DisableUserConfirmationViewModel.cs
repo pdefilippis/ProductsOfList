b322a49b96c909b723de38933a9770cbf7abf629
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-04-26 00:24:49 -0300

    Se agrega validators de Usuario

---
+++ Ecommerce/Ecommerce/ViewModels/Users/DisableUserConfirmationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.ViewModels.Users
+{
+    public class DisableUserConfirmationViewModel
+    {
+        [HiddenInput]
+        public int UserId { get; set; }
+        public string Username { get; set; }
+    }
+}
