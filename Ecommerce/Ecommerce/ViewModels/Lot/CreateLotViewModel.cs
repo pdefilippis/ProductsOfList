de7ae50aac98524e1802a122927d1154b1f05a08
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-04-28 18:57:45 -0300

    cambio de nombre propiedad imagen

--- Ecommerce/Ecommerce/ViewModels/Lot/CreateLotViewModel.cs
+++ Ecommerce/Ecommerce/ViewModels/Lot/CreateLotViewModel.cs
@@ -12,10 +12,10 @@ namespace Ecommerce.ViewModels.Lot
         [MaxLength(30, ErrorMessage = "Permite hasta 30 caracteres")]
         [Display(Name = "Descripción")]
         [Required(ErrorMessage = "El campo 'Descripción' es obligatorio.")]
-        public string Description { get; set; }
+        public string Descripcion { get; set; }
 
         [Display(Name = "Imagen")]
-        public IFormFile Image { get; set; }
+        public IFormFile Imagen { get; set; }
 
         public int LotId { get; set; }
     }
