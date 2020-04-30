d5b0984f34c190b372c5e0ae7cdb64df9900c452
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-04-29 23:59:43 -0300

    se corrigue msj de error

--- Ecommerce/Ecommerce/ViewModels/Login/LoginViewModel.cs
+++ Ecommerce/Ecommerce/ViewModels/Login/LoginViewModel.cs
@@ -14,18 +14,18 @@ namespace Ecommerce.ViewModels.Login
 
         public class InputModel
         {
-            [Required(ErrorMessage = "<font color='red'>El campo 'Email' es obligatorio.</font>")]
-            [EmailAddress(ErrorMessage = "<font color='red'>El 'Email' no es una dirección de Email válida.</font>")]
+            [Required(ErrorMessage = "El campo 'Email' es obligatorio.")]
+            [EmailAddress(ErrorMessage = "El 'Email' no es una dirección de Email válida.")]
             public string Email { get; set; }
-            [Required(ErrorMessage = "<font color='red'>El campo 'Contraseña' es obligatorio.</font>")]
+            [Required(ErrorMessage = "El campo 'Contraseña' es obligatorio.")]
             [DataType(DataType.Password)]
-            [StringLength(100, ErrorMessage = "<font color='red'>El número de caracteres del {0} debe ser al menos {2}.</font>", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "El número de caracteres del {0} debe ser al menos {2}.", MinimumLength = 6)]
             public string Password { get; set; }
-            [Required(ErrorMessage = "<font color='red'>El campo 'Usuario' es obligatorio.</font>")]
+            [Required(ErrorMessage = "El campo 'Usuario' es obligatorio.")]
             public string User { get; set; }
-            [Required(ErrorMessage = "<font color='red'>El campo 'Apellido' es obligatorio.</font>")]
+            [Required(ErrorMessage = "El campo 'Apellido' es obligatorio.")]
             public string Surname { get; set; }
-            [Required(ErrorMessage = "<font color='red'>El campo 'Nombre' es obligatorio.</font>")]
+            [Required(ErrorMessage = "El campo 'Nombre' es obligatorio.")]
             public string Name { get; set; }
 
         }
