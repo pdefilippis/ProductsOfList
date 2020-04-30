ca9b7bdcd485e048921b877f1ec9f1f1ce052d46
pdefilippis <pdefilippis@gmail.com>
2020-04-30 19:01:52 -0300

    no message

--- Ecommerce/Ecommerce/Controllers/AccountController.cs
+++ Ecommerce/Ecommerce/Controllers/AccountController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Ecommerce.Tables;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
