08d3c97512cc404c42c4302463f2bc5e66959fab
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-05-03 23:30:08 -0300

    se comenta marca

--- Ecommerce/Ecommerce/ViewModels/Article/EditArticleViewModel.cs
+++ Ecommerce/Ecommerce/ViewModels/Article/EditArticleViewModel.cs
@@ -15,10 +15,10 @@ namespace Ecommerce.ViewModels.Article
         [MaxLength(30, ErrorMessage = "Permite hasta 30 caracteres")]
         [Display(Name = "Marca")]
         [Required(ErrorMessage = "El campo 'Marca' es obligatorio.")]
-        public string Brand { get; set; }
-        [MaxLength(30, ErrorMessage = "Permite hasta 30 caracteres")]
-        [Display(Name = "Número de serie")]
-        [Required(ErrorMessage = "El campo 'Número de serie' es obligatorio.")]
+        //public string Brand { get; set; }
+        //[MaxLength(30, ErrorMessage = "Permite hasta 30 caracteres")]
+        //[Display(Name = "Número de serie")]
+        //[Required(ErrorMessage = "El campo 'Número de serie' es obligatorio.")]
         public string SerialNumber { get; set; }
         [MaxLength(50, ErrorMessage = "Permite hasta 50 caracteres")]
         [Display(Name = "Descripción")]
@@ -34,6 +34,6 @@ namespace Ecommerce.ViewModels.Article
         [Required(ErrorMessage = "El campo 'Precio' es obligatorio.")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:#.#}")]
         [RegularExpression(@"^[1-9]\d*$", ErrorMessage = "Debe ingresar números mayor a cero")]
-        public int? Price { get; set; }
+        public decimal Price { get; set; }
     }
 }
