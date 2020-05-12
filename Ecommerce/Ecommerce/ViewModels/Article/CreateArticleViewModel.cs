26544efcfae697bdd47e4674f0e038d50cbd7364
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-05-12 00:08:02 -0300

    se agrega propiedad marca

--- Ecommerce/Ecommerce/ViewModels/Article/CreateArticleViewModel.cs
+++ Ecommerce/Ecommerce/ViewModels/Article/CreateArticleViewModel.cs
@@ -14,12 +14,12 @@ namespace Ecommerce.ViewModels.Article
         [Required(ErrorMessage = "El campo 'Tipo' es obligatorio.")]
         public int TypeId { get; set; }
         [MaxLength(30, ErrorMessage = "Permite hasta 30 caracteres")]
+        [Display(Name = "Marca")]
+        [Required(ErrorMessage = "El campo 'Marca' es obligatorio.")]
+        public string Brand { get; set; }
+        [MaxLength(30, ErrorMessage = "Permite hasta 30 caracteres")]
         [Display(Name = "Numero de serie")]
         [Required(ErrorMessage = "El campo 'Numero de serie' es obligatorio.")]
-        //public string Brand { get; set; }
-        //[MaxLength(30, ErrorMessage = "Permite hasta 30 caracteres")]
-        //[Display(Name = "Número de serie")]
-        //[Required(ErrorMessage = "El campo 'Número de serie' es obligatorio.")]
         public string SerialNumber { get; set; }
         [MaxLength(50, ErrorMessage = "Permite hasta 50 caracteres")]
         [Display(Name = "Descripción")]
