59ca7306e641fb611dbac8e76ca6e2de40e1cb0e
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-05-13 11:37:07 -0300

    se crea metod getUsers para mostrar en la grilla

--- Ecommerce/Ecommerce/Controllers/UserController.cs
+++ Ecommerce/Ecommerce/Controllers/UserController.cs
@@ -1,19 +1,42 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ecommerce.Controllers
 {
     [Authorize]
     public class UserController : Controller
     {
+
+        private readonly Core.IUsuarioManager _usuarioManager;
+
+        public UserController(Core.IUsuarioManager usuarioManager)
+        {
+            _usuarioManager = usuarioManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public JsonResult GetUsers()
+        {
+            var user = _usuarioManager.GetAll();
+
+            var item = user.Select(u => new
+            {
+                user_id = u.Id,
+                username = u.UserName,
+                name = u.Nombre,
+                surname = u.Apellido,
+                mail = u.Email,
+                creation_timestamp = u.Creacion.ToString("dd/MM/yyyy HH:mm:ss"),
+                last_login_timestamp = u.UltimoIngreso.ToString("dd/MM/yyyy HH:mm:ss")
+            }).ToList();
+
+            return Json(item);
+    }
 
         public IActionResult CreateUser()
         {
