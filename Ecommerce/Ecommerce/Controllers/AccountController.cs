ad4bf2abd2e829ee5ee1ebc2c700d7dc6c504d89
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-05-17 20:36:49 -0300

    se agrega metodo para mostrar datos del usuario

--- Ecommerce/Ecommerce/Controllers/AccountController.cs
+++ Ecommerce/Ecommerce/Controllers/AccountController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using Ecommerce.ViewModels.Account;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,24 +7,29 @@ namespace Ecommerce.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private readonly Core.IUsuarioManager _usuarioManager;
+
+        public AccountController(Core.IUsuarioManager usuarioManager)
+        {
+            _usuarioManager = usuarioManager;
+        }
+
         public IActionResult UserData()
         {
-            //var CurrentUserName = int.Parse(HttpContext.User.Identity.Name);
+            var CurrentUserName = HttpContext.User.Identity.Name;
 
-            //var user = _usuarioManager.Get();
+            var user = _usuarioManager.GetByName(CurrentUserName);
 
-            //var _user = user.First(u => u.Id == CurrentUserName);
+            var userDataViewModel = new UserDataViewModel
+            {
+                Name = user.Nombre,
+                Surname = user.Apellido,
+                User = user.UserName,
+                Email = user.Email,
+                CreationTimestamp = user.Creacion.ToString(),
+            };
 
-            //var userDataViewModel = new UserDataViewModel
-            //{
-            //    Name = _user.Nombre,
-            //    Surname = _user.Apellido,
-            //    User = _user.UserName,
-            //    Email = _user.Email,
-            //    CreationTimestamp = _user.Creacion.ToString(),
-            //};
-            
-            return View();
+            return View(userDataViewModel);
         }
 
         public IActionResult ChangePassword()
