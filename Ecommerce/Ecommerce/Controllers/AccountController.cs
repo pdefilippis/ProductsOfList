03106d325b57854a918ff335f8b8e02da1ef55bf
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-05-17 12:45:35 -0300

    userdata

--- Ecommerce/Ecommerce/Controllers/AccountController.cs
+++ Ecommerce/Ecommerce/Controllers/AccountController.cs
@@ -13,6 +13,21 @@ namespace Ecommerce.Controllers
     {
         public IActionResult UserData()
         {
+            //var CurrentUserName = int.Parse(HttpContext.User.Identity.Name);
+
+            //var user = _usuarioManager.Get();
+
+            //var _user = user.First(u => u.Id == CurrentUserName);
+
+            //var userDataViewModel = new UserDataViewModel
+            //{
+            //    Name = _user.Nombre,
+            //    Surname = _user.Apellido,
+            //    User = _user.UserName,
+            //    Email = _user.Email,
+            //    CreationTimestamp = _user.Creacion.ToString(),
+            //};
+            
             return View();
         }
 
