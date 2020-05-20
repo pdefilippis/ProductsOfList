1435f886340e31bd4badb6fe0739f95642826889
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-05-20 00:12:15 -0300

    se agrega js de editar usuario

---
+++ Ecommerce/Ecommerce/wwwroot/js/User/EditUser.js
@@ -0,0 +1,26 @@
+﻿(function (self, $, undefined) {
+    var is_admin_check = $("#IsAdmin");
+    var is_admin_toggle_button = $('#is-admin-toggle-button');
+
+    EditUser.Start = function () {
+
+
+        is_admin_toggle_button.on("click", function () {
+            is_admin_check.bootstrapToggle('toggle');
+        });
+    }
+
+    EditUser.Check = function () {
+        if ($("#IsAdmin").is(":checked")) {
+            is_admin_check.bootstrapToggle('enable');
+            is_admin_toggle_button.prop('disabled', false);
+        } else {
+            is_admin_check.bootstrapToggle('disabled');
+            is_admin_toggle_button.prop('enable', true);
+        }
+    }
+}(window.EditUser = window.EditUser || {}, jQuery));
+
+jQuery(function () {
+    EditUser.Start();
+})
