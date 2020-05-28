6d80901bfe0225ef5aec2754a4325e11dc4a3153
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-05-28 02:18:45 -0300

    se agrega js de paginacion de lotes

---
+++ Ecommerce/Ecommerce/wwwroot/js/Home/Index.js
@@ -0,0 +1,30 @@
+﻿(function (self, $, undefined) {
+
+    Index.Start = function () {
+
+
+        $('.owl-carousel').owlCarousel({
+            //loop: true,
+            dots: true,
+            nav: true,
+            navText: ["<i class='glyphicon glyphicon-chevron-left' style='color:#005eb8; font-size: 2em; line-height:unset'></i>", "<span class='glyphicon glyphicon-chevron-right' style='color:#005eb8; font-size: 2em; line-height:unset'></span>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        });
+
+    };
+
+}(window.Index = window.Index || {}, jQuery));
+
+jQuery(function () {
+    Index.Start();
+});
