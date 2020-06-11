b903247d359874eb3ab88d29ced3d8ed9309792e
pdefilippis <33737681+pdefilippis@users.noreply.github.com>
2020-06-11 17:00:19 -0300

    Update ProductsManagerContext.cs
    Se comenta cadena de conexion

--- Ecommerce/Ecommerce.EF/Models/ProductsManagerContext.cs
+++ Ecommerce/Ecommerce.EF/Models/ProductsManagerContext.cs
@@ -29,7 +29,7 @@ namespace Ecommerce.Domain.Models
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;database=ProductsManager;user=usrpm;password=usrpm");
+                //optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;database=ProductsManager;user=usrpm;password=usrpm");
             }
         }
 
