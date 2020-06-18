3b298814f9e464fbc1a2721f7d7b788792d0aac9
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-06-18 18:34:48 -0300

    se quita campo id

--- Ecommerce/Ecommerce/wwwroot/js/Article/IndexPublic.js
+++ Ecommerce/Ecommerce/wwwroot/js/Article/IndexPublic.js
@@ -16,11 +16,6 @@
             order: [[0, "desc"]],
             columns: [
                 {
-                    title: $("#id-column-title").html(),
-                    data: "article_id",
-                    responsivePriority: 1
-                },
-                {
                     title: $("#articleDescription-column-title").html(),
                     data: "article_Description",
                     render: $.fn.dataTable.render.text(),
