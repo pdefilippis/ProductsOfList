584a2c50555c2d4a9758e10a593b932a8a723cf4
pdifonzo95 <49298344+pdifonzo95@users.noreply.github.com>
2020-06-25 01:26:29 -0300

    se implementa vm de contactanos

---
+++ Ecommerce/Ecommerce/ViewModels/Home/ContactUsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.ViewModels.Home
+{
+    public class ContactUsViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [EmailAddress(ErrorMessage = "Formato inválido")]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
